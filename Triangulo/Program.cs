19c5e66efbad4fa00a7cc6130423deaf73f5f3f6
Victor Muniz <victor.hm2000@aluno.ifsc.edu.br>
2021-05-10 15:14:58 -0300

    Primeiro commit

---
+++ Triangulo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo triangulo1 = new Triangulo(5, 5, 5);
+            Triangulo triangulo2 = new Triangulo(5, 5, 6);
+            Triangulo triangulo3 = new Triangulo(5, 7, 8);
+            Triangulo triangulo4 = new Triangulo(1, 2, 5);
+
+            Triangulo triangulo5 = new Triangulo(5, 6, 5);
+            Triangulo triangulo6 = new Triangulo(6, 5, 5);
+
+            Console.WriteLine(triangulo1.MostrarTipo());
+            Console.WriteLine(triangulo2.MostrarTipo());
+            Console.WriteLine(triangulo5.MostrarTipo());
+            Console.WriteLine(triangulo6.MostrarTipo());
+            Console.WriteLine(triangulo3.MostrarTipo());
+            Console.WriteLine(triangulo4.MostrarTipo());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Triangulo/Triangulo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangulo
+{
+    class Triangulo
+    {
+        private readonly double a, b, c;
+        private string resultadoValidacao = "";
+
+        public Triangulo (double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public string MostrarTipo()
+        {
+            ValidarTriangulo();
+            return $"O triângulo ({a}, {b}, {c}) é um triângulo {resultadoValidacao}.";
+        }
+
+        #region Métodos privados
+        private string ValidarTriangulo()
+        {
+            if (a + b <= c || a + c <= b || b + c <= a)
+                resultadoValidacao += "INVÁLIDO";
+            else
+            {
+                if (this.a == this.b && this.b == this.c)
+                    resultadoValidacao += "equilátero";
+                else if (a == b || a == c || b == c)
+                    resultadoValidacao += "isósceles";
+                else
+                    resultadoValidacao += "escaleno";
+            }
+
+            return resultadoValidacao;
+        }
+        #endregion
+    }
+}
